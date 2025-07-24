9a1dd3d3ed7c7601758c06817b5c3a39a9b260f0
kalpashree17 <kalpashreesharma96@gmail.com>
2025-07-24 14:30:56 +0545

    new folders added

---
+++ oop/class/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+public class Teacher
+{
+    public string Name;
+    public int idNo;
+    public int Age;
+
+    public void changeDep()
+    {
+        Console.WriteLine("Enter the new department name:");
+        string newDepartment = Console.ReadLine();
+        Console.WriteLine($"{Name}'s Department changed to: {newDepartment}");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Teacher teacher1 = new Teacher();
+
+        teacher1.Name = "Sagar KC";
+        teacher1.idNo = 50;  
+
+        teacher1.changeDep();
+    }
+}
