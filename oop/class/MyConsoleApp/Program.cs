c0e84cb82430f188da8b367aaeb6c468674a41e1
kalpashree17 <kalpashreesharma96@gmail.com>
2025-07-26 19:52:51 +0545

    Jagged array used

---
+++ oop/class/MyConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+
+
+public class Teacher
+{
+    public string Name;
+    public int idNo;
+    public int Age;
+
+    public void changeDep()
+    {
+        Console.WriteLine("Enter the new department name:");
+        string newDepartment = Console.ReadLine();
+        Console.WriteLine($"{Name}'s Department changed to:{newDepartment}");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+
+        JaggedArray demo = new JaggedArray();
+        demo.Display();
+
+        Console.WriteLine("Enter Array size:");
+        int n = int.Parse(Console.ReadLine());
+        string[] courses = new string[n];
+        //reading array
+        for (int i = 0; i < courses.Length; i++)
+        {
+            Console.WriteLine("Enter course name:");
+            courses[i] = Console.ReadLine();
+        }
+
+        //printing the values
+        for (int i = 0; i < courses.Length; i++)
+        {
+            Console.WriteLine($"Course {i + 1}: {courses[i]}");
+        }
+        //printing using foreach--use only for printing 
+        foreach (string course in courses)
+        {
+            Console.WriteLine($"Course: {course}");
+        }
+
+        Teacher teacher1 = new Teacher();
+
+        teacher1.Name = "Sagar KC";
+        teacher1.idNo = 50;
+
+        teacher1.changeDep();
+    }
+}
---
+++ oop/class/MyConsoleApp/jagged.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class JaggedArray
+{
+    public void Display()
+    {
+        // Creating a jagged array for theater seating
+        string[][] seats = new string[4][];
+
+        seats[0] = new string[] { "A1", "A2", "A3", "A4" };
+        seats[1] = new string[] { "B1", "B2", "B3", "B4", "B5" };
+        seats[2] = new string[] { "C1", "C2" };
+        seats[3] = new string[] { "D1", "D2", "D3" };
+
+        // Displaying the seating
+        for (int i = 0; i < seats.Length; i++)
+        {
+            for (int j = 0; j < seats[i].Length; j++)
+            {
+                Console.Write(seats[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
