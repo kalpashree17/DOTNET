e34a65493100332adfef030b7e3be532ed23d1f2
kalpashree17 <kalpashreesharma96@gmail.com>
2025-10-28 21:28:02 +0545

    lab-2 completed:string case conversion

---
+++ oop/lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// to convert input strings from lower to upper and upper to lower case
+
+using System;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Lab 2 ===");
+            Console.WriteLine("Program started...\n");
+
+            Console.WriteLine("Enter the string to convert from lower to uppercase: ");
+            string input = Console.ReadLine();
+
+            string upper = input.ToUpper();
+            Console.WriteLine("The converted output is : " + upper);
+
+            Console.WriteLine("Enter the string to convert from upper to lowercase: ");
+            string input2 = Console.ReadLine();
+
+            string lower = input2.ToLower();
+            Console.WriteLine("The converted output is : " + lower);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
